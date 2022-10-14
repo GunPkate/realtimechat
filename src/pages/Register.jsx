195452d026433611de02dbb73a8a0ef1ff8bf36e
GunPKate <gun@builk.com>
2022-10-14 21:08:25 +0700

    add image and style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Add from "../img/addAvatar.png"
 
 const Register = () =>{
     return (
@@ -10,7 +11,11 @@ const Register = () =>{
                     <input type="text" placeholder="display name"/>
                     <input type="email" placeholder="email"/>
                     <input type="password" placeholder="password"/>
-                    <input type="file"/>
+                    <input style={{display:"none"}} type="file" id="file"/>
+                    <label htmlFor="file">
+                        <img src={Add} alt=""/>
+                        <span>Add an avatar</span>
+                    </label>
                     <button>Sign Up</button>
                 </form>
                 <p>Do you have an account? Login</p>
