ba33f5a344c2e49f25d10c6107475e1a06fd23a9
GunPKate <gun@builk.com>
2022-10-16 21:17:58 +0700

    complete ux/ui for chat

--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>
-      mesage
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/10272672/pexels-photo-10272672.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+      <img
+          src="https://images.pexels.com/photos/10272672/pexels-photo-10272672.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
