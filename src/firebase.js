d1f13cb15669cb3198bf9977c87e091b2ca424b2
GunPKate <gun@builk.com>
2022-10-18 22:22:35 +0700

    save name email  and image in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage, ref } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDT7korTvjW42sgut_Qi8a9cbhMyyrjeVM",
@@ -14,3 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Add from "../img/addAvatar.png";
-import { auth } from "../firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, storage, db } from "../firebase.js";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
     e.preventDefault(); // prevent refresh
     // console.log(e.target[0].value);
     const displayName = e.target[0].value;
@@ -12,18 +15,36 @@ const Register = () => {
     const password = e.target[2].value;
     const file = e.target[3].value;
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observer
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName: displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db), "users", res.user.uid, {
+              uid: res.user.uid,
+              displayName: displayName,
+              email: email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (error) {
+      setErr(true);
+    }
   };
 
   return (
@@ -31,7 +52,7 @@ const Register = () => {
       <div className="formWrapper">
         <span className="logo">Nanosoft Team</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
@@ -41,6 +62,7 @@ const Register = () => {
             <span>Add an avatar</span>
           </label>
           <button>Sign Up</button>
+          {err && <span>Something went wrong</span>}
         </form>
         <p>Do you have an account? Login</p>
       </div>
